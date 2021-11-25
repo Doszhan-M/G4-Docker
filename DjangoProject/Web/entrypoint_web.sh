782730349c9e156335be7a49b76ffa6186bc1ccd
Doszhan <doszhan.m@mail.ru>
2021-11-25 22:35:59 +0600

    add nginx

--- DjangoProject/Web/entrypoint_web.sh
+++ DjangoProject/Web/entrypoint_web.sh
@@ -5,7 +5,7 @@ python3 manage.py migrate
 
 if [ $DEPLOY = "TRUE" ]; then
 echo "DEPLOY"
-gunicorn Web.wsgi:application --bind 0.0.0.0:8000
+gunicorn Web.wsgi:application --bind 0.0.0.0:8001
 else
 echo "Running test"
 python3 manage.py runserver 0.0.0.0:8000
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 admin@admin.com
 пароль django777
 
-http://localhost:8000/
+http://localhost/
 
 docker-compose up --build
 docker-compose -f docker-compose.dev.yml up --build
